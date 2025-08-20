cd2260c067211f1700d527c7adb62cb4d7996bb6
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-20 22:08:58 +0530

    hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
     <>
-      <div className='dark:bg-black relative'>
+      <div className='dark:bg-black/90 relative'>
         <Navbar theme={theme} setTheme={setTheme} />
+        <Hero />
       </div>
     </>
   )
