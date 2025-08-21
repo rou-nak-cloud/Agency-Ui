59a1bbba050d82bb1c9bbc26077936829667c057
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-21 22:16:41 +0530

    Footer initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className='bg-slate-100 dark:bg-gray-900 pt-5 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-12 lg:px-24 xl:px-40'>
+        {/* Footer top */}
+        <div className='flex justify-between sm:items-center max-md:flex-col gap-10'>
+            <div></div>
+            <div></div>
+        </div>
+        {/* Footer bottom */}
+        
+     </div>
+    </>
+  )
+}
+
+export default Footer
