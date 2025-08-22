d7cfbc079b275d49425edd3654ae712236a6d5e3
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-22 16:20:58 +0530

    custom cursor movement style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
@@ -12,6 +12,39 @@ import Footer from './components/Footer'
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
+
+  const dotRef =  useRef(null)
+  const outerRingRef =  useRef(null)
+
+  // Refs for custom cursor position tracking
+  const mouse = useRef({x:0,y:0}) // actual mouse position (instant)
+  const position = useRef({x:0,y:0}) //a lagged/smooth position
+
+  useEffect(()=>{
+    const handleMouseMove = (e) => {
+      mouse.current.x = e.clientX
+      mouse.current.y = e.clientY
+    }
+
+    document.addEventListener('mousemove', handleMouseMove)
+    // smoothing / lag effect:
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.05
+      position.current.y += (mouse.current.y - position.current.y) * 0.05
+
+      if(dotRef.current && outerRingRef.current){
+        dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y - 6}px, 0)`
+        outerRingRef.current.style.transform = `translate3d(${position.current.x - 10}px, ${position.current.y - 10}px, 0)`
+      }
+      requestAnimationFrame(animate) //creates a loop using requestAnimationFrame (optimized for 60fps).
+    }
+    animate()
+
+    return ()=> {
+      document.removeEventListener('mousemove', handleMouseMove) //Cleanup
+    }
+  },[])
+
   return (
     <>
       <div className='dark:bg-black/95 relative'>
@@ -24,6 +57,13 @@ const App = () => {
         <Teams />
         <Contact />
         <Footer theme={theme} />
+
+        {/* Mouse move animation */}
+          {/* custom curser outer ring */}
+          <div ref={outerRingRef} className='fixed top-0 left-0 w-5 h-5 rounded-full border border-primary pointer-events-none z-[9999]'></div>
+
+          {/* custom cursor dot */}
+          <div ref={dotRef} className='fixed top-0 left-0 w-3 h-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
       </div>
     </>
   )
